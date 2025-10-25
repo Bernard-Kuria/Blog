0597412923110b897cf773b403a38cdfd64d2f6c
Bernard-Kuria <bernard.i.kuria@gmail.com>
2025-10-25 13:08:30 +0300

    Debugging consoles removed.

--- components/Draftify.jsx
+++ components/Draftify.jsx
@@ -37,8 +37,6 @@ export default function Draftify({ data }) {
     whileHover,
   } = useDraftify(draft);
 
-  console.log(blocksData);
-
   useEffect(() => {
     setMounted(true);
     setDraft([]);
