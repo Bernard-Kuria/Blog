0162deeb21772e9a2eb717a207d9fcc94b093a6e
Bernard-Kuria <bernard.i.kuria@gmail.com>
2025-10-27 00:27:51 +0300

    mismatch again

--- app/api/blogs/[id]/route.ts
+++ app/api/blogs/[id]/route.ts
@@ -12,7 +12,7 @@ import {
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: string } } // <-- plain object
 ) {
   try {
     const { id } = params;
